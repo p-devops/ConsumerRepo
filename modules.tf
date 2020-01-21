6b42009b06b1dc69cc85173ebefdf01761534ead
peter leung <49036546+p-devops@users.noreply.github.com>
2020-01-20 23:03:42 -0500

    Create modules.tf

---
+++ modules.tf
@@ -0,0 +1,24 @@
+//--------------------------------------------------------------------
+// Variables
+
+
+
+//--------------------------------------------------------------------
+// Modules
+module "consumer_ec2_instance" {
+  source  = "app.terraform.io/p-devops/consumer-ec2-instance/aws"
+  version = "1.24.0"
+
+  instance_count = 2
+  name = "p-devops-ec2"
+}
+
+module "consumer_elb" {
+  source  = "app.terraform.io/p-devops/consumer-elb/aws"
+  version = "1.24.0"
+
+  environment = "${var.environment}"
+  instances = "${module.consumer_ec2_instance.id"
+  name = "p-devops-ELB"
+  number_of_instances = 2
+}
